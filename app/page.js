e5135dc0c10a2cdff72c3950bb1127256ef9a093
upathsg <upathsg@gmail.com>
2026-05-01 22:05:39 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import Link from 'next/link';
 import ArticleCard from '@/components/ArticleCard';
 import { getLatestPosts, getPostCount } from '@/lib/api';
