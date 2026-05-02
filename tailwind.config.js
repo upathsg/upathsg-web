13fd1133575a57b0e6df206b404114e85408367d
upathsg <upathsg@gmail.com>
2026-05-02 15:09:28 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        pink: '#ff304f',
-        teal: '#00798c',
-        navy: '#173f5f',
-        peach: '#fae8d8',
-        'peach-light': '#fdf4ee',
-      },
+  pink: '#D79B98',
+  'pink-light': '#F0C8C8',
+  teal: '#50A08C',
+  'teal-light': '#3CB48C',
+  navy: '#0F1932',
+  peach: '#F5E6D7',
+  'peach-light': '#F0E6CD',
+  gray: '#999999',
+  'gray-light': '#efefef',
+  blue: '#20639B',
+  'blue-light': '#497fad',
+},
       fontFamily: {
         nunito: ['Nunito', 'sans-serif'],
         lato: ['Lato', 'sans-serif'],
